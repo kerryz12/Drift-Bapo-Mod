a1c79e3643facaecdb4e2b92c4a19707eb515fbb
BuildTools <unconfigured@null.spigotmc.org>
2020-12-27 23:42:38 -0800

    added all leaves and logs, added limit

--- src/main/java/com/driftbapo/driftbapomod/common/tools/BrianAxe.java
+++ src/main/java/com/driftbapo/driftbapomod/common/tools/BrianAxe.java
@@ -17,58 +17,68 @@ import net.minecraft.util.ActionResultType;
 
 public class BrianAxe extends AxeItem {
 
+    private int counter;
+    private static final int BLOCK_BREAK_LIMIT = 1000;
+
     public BrianAxe (IItemTier itier, int ad, float atkspd, Properties builder){
         super(itier, ad, atkspd, builder);
+        counter = 0;
     }
 
     @Override
     public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity p, Hand handIn){
-        double x = p.getPosX();
-        double y = p.getPosY();
-        double z = p.getPosZ();
-
+        counter = 0;
         ItemStack itemStackIn = p.getHeldItem(handIn);
         BlockRayTraceResult rayTraceResult = this.rayTrace(worldIn, p, RayTraceContext.FluidMode.NONE);
         BlockPos origin = rayTraceResult.getPos();
 
-        if (worldIn.getBlockState(origin).getBlock().equals(Blocks.STRIPPED_OAK_LOG) || worldIn.getBlockState(origin).getBlock().equals(Blocks.OAK_LOG)
-                || worldIn.getBlockState(origin).getBlock().equals(Blocks.BIRCH_LOG) || worldIn.getBlockState(origin).getBlock().equals(Blocks.OAK_LEAVES)
-                || worldIn.getBlockState(origin).getBlock().equals(Blocks.ACACIA_LOG) || worldIn.getBlockState(origin).getBlock().equals(Blocks.JUNGLE_LOG)) {
-
+        if (this.isTree(worldIn, origin)) {
             worldIn.destroyBlock(origin, true);
-
             helper_function(worldIn, p, handIn, origin.add(1, 0, 0));
             helper_function(worldIn, p, handIn, origin.add(-1, 0, 0));
             helper_function(worldIn, p, handIn, origin.add(0, 1, 0));
             helper_function(worldIn, p, handIn, origin.add(0, -1, 0));
             helper_function(worldIn, p, handIn, origin.add(0, 0, 1));
             helper_function(worldIn, p, handIn, origin.add(0, 0, -1));
-
         }
 
         return ActionResult.resultPass(itemStackIn);
 
     }
 
-    public ActionResult<ItemStack> helper_function(World worldIn, PlayerEntity p, Hand handIn, BlockPos origin) {
+    private ActionResult<ItemStack> helper_function(World worldIn, PlayerEntity p, Hand handIn, BlockPos origin) {
         ItemStack itemStackIn = p.getHeldItem(handIn);
 
-        if (worldIn.getBlockState(origin).getBlock().equals(Blocks.STRIPPED_OAK_LOG) || worldIn.getBlockState(origin).getBlock().equals(Blocks.OAK_LOG)
-                || worldIn.getBlockState(origin).getBlock().equals(Blocks.BIRCH_LOG) || worldIn.getBlockState(origin).getBlock().equals(Blocks.OAK_LEAVES)
-                || worldIn.getBlockState(origin).getBlock().equals(Blocks.ACACIA_LOG) || worldIn.getBlockState(origin).getBlock().equals(Blocks.JUNGLE_LOG)) {
-
+        if (isTree(worldIn, origin)) {
+            counter++;
+            if (counter > BLOCK_BREAK_LIMIT){
+                return ActionResult.resultFail(itemStackIn);
+            }
             worldIn.destroyBlock(origin, true);
-
             helper_function(worldIn, p, handIn, origin.add(1, 0, 0));
             helper_function(worldIn, p, handIn, origin.add(-1, 0, 0));
             helper_function(worldIn, p, handIn, origin.add(0, 1, 0));
             helper_function(worldIn, p, handIn, origin.add(0, -1, 0));
             helper_function(worldIn, p, handIn, origin.add(0, 0, 1));
             helper_function(worldIn, p, handIn, origin.add(0, 0, -1));
-
         }
 
         return ActionResult.resultPass(itemStackIn);
+    }
+
+    private static boolean isTree(World worldIn, BlockPos origin){
+        if(worldIn.getBlockState(origin).getBlock().equals(Blocks.STRIPPED_OAK_LOG) || worldIn.getBlockState(origin).getBlock().equals(Blocks.OAK_LOG)
+            || worldIn.getBlockState(origin).getBlock().equals(Blocks.STRIPPED_BIRCH_LOG) || worldIn.getBlockState(origin).getBlock().equals(Blocks.BIRCH_LOG)
+            || worldIn.getBlockState(origin).getBlock().equals(Blocks.STRIPPED_ACACIA_LOG) || worldIn.getBlockState(origin).getBlock().equals(Blocks.ACACIA_LOG)
+            || worldIn.getBlockState(origin).getBlock().equals(Blocks.STRIPPED_DARK_OAK_LOG) || worldIn.getBlockState(origin).getBlock().equals(Blocks.DARK_OAK_LOG)
+            || worldIn.getBlockState(origin).getBlock().equals(Blocks.STRIPPED_JUNGLE_LOG) || worldIn.getBlockState(origin).getBlock().equals(Blocks.JUNGLE_LOG)
+            || worldIn.getBlockState(origin).getBlock().equals(Blocks.STRIPPED_SPRUCE_LOG) || worldIn.getBlockState(origin).getBlock().equals(Blocks.SPRUCE_LOG)
+            || worldIn.getBlockState(origin).getBlock().equals(Blocks.OAK_LEAVES) || worldIn.getBlockState(origin).getBlock().equals(Blocks.DARK_OAK_LEAVES)
+            || worldIn.getBlockState(origin).getBlock().equals(Blocks.SPRUCE_LEAVES) || worldIn.getBlockState(origin).getBlock().equals(Blocks.ACACIA_LEAVES)
+            || worldIn.getBlockState(origin).getBlock().equals(Blocks.JUNGLE_LEAVES) || worldIn.getBlockState(origin).getBlock().equals(Blocks.BIRCH_LEAVES)){
+            return true;
+        }
+        return false;
     }
 
 }
